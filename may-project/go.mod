4b8f827d3d2fb59700840f23495d9ae0e4525f50
victor ejike <victorejiki@gmail.com>
2026-05-03 18:31:20 +0100

    ascii art

---
+++ may-project/go.mod
@@ -0,0 +1,3 @@
+module may-project
+
+go 1.22.2
--- may-project/main.go
+++ may-project/main.go
@@ -3,18 +3,36 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
-func main(){
-	if len(os.Args) < 2 || len(os.Args) > 3 {
-		fmt.Println("usage: go run main. [string] [banner]")
+func main() {
+	if len(os.Args) < 2 || os.Args[1] == "" {
 		return
 	}
+	content, _ := os.ReadFile("standard.txt")
+	banner := strings.Split(string(content), "\n")
 
-	input := os.Args[1]
-	bannerName := "standard"
+	input := strings.ReplaceAll(os.Args[1], "\\n", "\n")
+	parts := strings.Split(input, "\n")
 
-	if len(os.Args) != 3 {
-		bannerName = 
+	for _, word := range parts {
+		if word == "" {
+			fmt.Println()
+			continue
+		}
+		for i := 0; i < 9; i++ {
+			for _, r := range word {
+				// Each char block in file is 9 lines (8 art + 1 empty)
+				// Index = (char - 32) * 9 + 1 (to skip initial file newline)
+				idx := int(r-32)*9 + 1 + i
+				if idx < len(banner) && i < 8 {
+					fmt.Print(banner[idx])
+				} else if i == 8 {
+					fmt.Print("") // The 9th line
+				}
+			}
+			fmt.Println()
+		}
 	}
 }
